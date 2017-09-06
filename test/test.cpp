7cba2250c4a007b69e2729e4e9887597a91662e4
godwings <godwings@outlook.com>
2017-09-06 20:02:46 +0800

    Finish Node

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,9 @@ Date: 06/09/2017
 */
 #include <cctype>
 #include <cstdio>
+#include <algorithm>
+using std::max;
+using std::swap;
 
 namespace FastIO {
     template <class T>
@@ -31,6 +34,111 @@ namespace FastIO {
     }
 }  // namespace FastIO
 using FastIO::scan;
+
+template<typename T>
+struct LinkCutTree {
+    enum Relation {
+        L, R
+    };
+    struct Node {
+        bool reversed;
+        T value, max_val;
+        typedef Node* ptrNode;
+        ptrNode child[2], parent, path_parent;
+        Node() {
+            value = max_val;
+            child[L] = child[R] = parent = path_parent = NULL;
+        }
+        Node(const int value_) {
+            value = max_val = value_;
+            child[L] = child[R] = parent = path_parent = NULL;
+        }
+        Relation GetRelation() {
+            return this == parent->child[L] ? L : R;
+        }
+        void Maintain() {
+            max_val = value;
+            if (child[L])
+                max_val = max(max_val, child[L]->max_val);
+            if (child[R])
+                max_val = max(max_val, child[R]->max_val);
+        }
+        void PushDown() {
+            if (reversed) {
+                swap(child[L], child[R]);
+                if (child[L])
+                    child[L]->reversed ^= 1;
+                if (child[R])
+                    child[R]->reversed ^= 1;
+                reversed = false;
+            }
+        }
+        void Rotate() {
+            swap(path_parent, parent->path_parent);
+            const ptrNode OldParent = parent;
+            const Relation now = GetRelation();
+            if (OldParent->parent)
+                OldParent->parent->child[OldParent->GetRelation()] = this;
+            parent = OldParent->parent;
+            OldParent->parent = this;
+            OldParent->child[now] = child[now ^ 1];
+            if (child[now ^ 1])
+                child[now ^ 1]->parent = OldParent;
+            OldParent->parent = this;
+            child[now ^ 1] = OldParent;
+            OldParent->Maintain();
+            Maintain();
+        }
+        void Splay() {
+            while (parent) {
+                if (!parent->parent) {
+                    parent->PushDown();
+                    Rotate();
+                } else {
+                    parent->parent->PushDown();
+                    parent->PushDown();
+                    if (GetRelation() == parent->GetRelation()) {
+                        parent->Rotate();
+                        Rotate();
+                    } else {
+                        Rotate();
+                        Rotate();
+                    }
+                }
+            }
+        }
+        void Expose() {
+            Splay();
+            PushDown();
+            if (child[R]) {
+                child[R]->parent = NULL;
+                child[R]->path_parent = this;
+                child[R] = NULL;
+                Maintain();
+            }
+        }
+        void Splice() {
+            Splay();
+            if (!path_parent)
+                return false;
+            path_parent->Expose();
+            path_parent->child[R] = this;
+            parent = path_parent;
+            path_parent = NULL;
+            parent->Maintain();
+            return true;
+        }
+        void Access() {
+            Expose();
+            while (Splice()) { }
+        }
+        void MakeRoot() {
+            Access();
+            Splay();
+            reversed ^= 1;
+        }
+    };
+};
 
 int main() {
 #ifndef ONLINE_JUDGE

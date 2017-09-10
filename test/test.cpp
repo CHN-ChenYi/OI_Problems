0cb5cd31407f1200254f22c3a2546ae9d214a75e
godwings_thinkpad_desktop <godwings@outlook.com>
2017-09-10 18:17:35 +0800

    Finish read

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,11 @@ Date: 10/09/2017
 */
 #include <cctype>
 #include <cstdio>
+#include <algorithm>
+const int kMaxN = 100010;
+typedef long long LL;
+const LL inf = 0x7fffffffffffffff;
+using std::min;
 
 namespace FastIO {
     template <class T>
@@ -21,6 +26,9 @@ namespace FastIO {
         if (!positive)
             x *= -1;
     }
+    inline void scan(char &x) {
+        while (!isalpha(x = getchar())) { }
+    }
     template <class T>
     inline void scan(T &x, T &y) {
         scan(x); scan(y);
@@ -29,8 +37,22 @@ namespace FastIO {
     inline void scan(T &x, T &y, T &z) {
         scan(x); scan(y); scan(z);
     }
+    template <class T>
+    inline void scan(T &a, T &b, T &c, T &d) {
+        scan(a); scan(b); scan(c); scan(d);
+    }
 }  // namespace FastIO
 using FastIO::scan;
+
+int n;
+char s[kMaxN];
+int cnt[kMaxN][26];
+int dif[kMaxN], sum[kMaxN];
+
+int a, b;
+int c, d, l, r;
+int q[kMaxN], pre[kMaxN];
+LL f[kMaxN];
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -41,6 +63,20 @@ int main() {
     freopen("string.out", "w", stdout);
 #endif  // _VISUAL_STUDIO
 #endif
+    scan(n);
+    scan(a, b);
+    scan(c, d, l, r);
+    for (int i = 1; i <= n; i++) {
+        scan(s[i]);
+        cnt[i][s[i] - 'a'] = 1;
+        for (int j = 0; j < 26; j++)
+            cnt[i][j] += cnt[i - 1][j];
+    }
+    for (int i = 1; i <= n; i++) {
+        f[i] = inf;
+        scan(dif[i]);
+        sum[i] = sum[i - 1] + dif[i];
+    }
 
     return 0;
 }

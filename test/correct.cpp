4bd0a09e4a5008631397077b804a0195442f0c56
godwings_thinkpad_desktop <godwings@outlook.com>
2017-09-12 15:12:38 +0800

    Rename var in correct

--- test/correct.cpp
+++ test/correct.cpp
@@ -7,76 +7,73 @@
 using namespace std;
 const int N = 1e5 + 10, Q = 5e3 + 10, lim = 1e9;
 struct point {
-    int x, num;
+    int x, id;
+    bool operator<(const point rhs) const {
+        return x < rhs.x;
+    }
 }a[N];
 struct query {
-    int x, y, k, cnt, num;
-}q[Q], b[Q];
-int sum[N], ans[Q];
+    int l, r, k, cnt, id;
+}q[Q], tmp[Q];
+int tr[N], ans[Q];
 int n, m;
-bool cmp(const point &aa, const point &bb) {
-    return aa.x < bb.x;
+inline int lowerbit(const int x) {
+    return x & -x;
 }
-void calc(int ll, int rr, int rawL, int mid) {
-    int L = 1, R = n + 1, MID;
-    while (L < R) {
-        MID = (L + R) >> 1;
-        if (a[MID].x >= rawL)
-            R = MID;
-        else
-            L = MID + 1;
+void Calc(int q_id_l, int q_id_r, int ans_l, int ans_r) {
+    const int pos = lower_bound(a + 1, a + n + 1, point{ans_l, 0}) - a;
+    for (int i = pos; i <= n && a[i].x <= ans_r; ++i) {
+        for (int j = a[i].id; j <= n; j += lowerbit(j))
+            ++tr[j];
     }
-    for (int i = R; i <= n && a[i].x <= mid; ++i)
-        for (int j = a[i].num; j <= n; j += (j & -j))
-            ++sum[j];
-    for (int i = ll; i <= rr; ++i) {
+    for (int i = q_id_l; i <= q_id_r; ++i) {
         q[i].cnt = 0;
-        for (int j = q[i].y; j; j -= (j & -j))
-            q[i].cnt += sum[j];
-        for (int j = q[i].x - 1; j; j -= (j & -j))
-            q[i].cnt -= sum[j];
+        for (int j = q[i].r; j; j -= lowerbit(j))
+            q[i].cnt += tr[j];
+        for (int j = q[i].l - 1; j; j -= lowerbit(j))
+            q[i].cnt -= tr[j];
     }
-    for (int i = R; i <= n && a[i].x <= mid; ++i)
-        for (int j = a[i].num; j <= n; j += (j & -j))
-            --sum[j];
+    for (int i = pos; i <= n && a[i].x <= ans_r; ++i) {
+        for (int j = a[i].id; j <= n; j += lowerbit(j))
+            --tr[j];
+    }
 }
-void divide(int ll, int rr, int rawL, int rawR) {
-    if (rawL == rawR) {
-        for (int i = ll; i <= rr; ++i)
-            ans[q[i].num] = rawR;
+void OverallDichotomy(int q_id_l, int q_id_r, int ans_l, int ans_r) {
+    if (ans_l == ans_r) {
+        for (int i = q_id_l; i <= q_id_r; ++i)
+            ans[q[i].id] = ans_r;
         return;
     }
-    int mid = rawL + ((rawR - rawL) >> 1);
-    calc(ll, rr, rawL, mid);
-    int now1 = ll, now2 = rr;
-    for (int i = ll; i <= rr; ++i) {
+    int ans_mid = ans_l + ((ans_r - ans_l) >> 1);
+    Calc(q_id_l, q_id_r, ans_l, ans_mid);
+    int left_ptr = q_id_l, right_ptr = q_id_r;
+    for (int i = q_id_l; i <= q_id_r; ++i) {
         if (q[i].cnt >= q[i].k)
-            b[now1++] = q[i];
+            tmp[left_ptr++] = q[i];
         else {
             q[i].k -= q[i].cnt;
-            b[now2--] = q[i];
+            tmp[right_ptr--] = q[i];
         }
     }
-    for (int i = ll; i <= rr; ++i)
-        q[i] = b[i];
-    if (now1 != ll)
-        divide(ll, now1 - 1, rawL, mid);
-    if (now2 != rr)
-        divide(now2 + 1, rr, mid + 1, rawR);
+    memcpy(q + q_id_l, tmp + q_id_l, (q_id_r - q_id_l + 1) * sizeof(query));
+    if (left_ptr != q_id_l)
+        OverallDichotomy(q_id_l, left_ptr - 1, ans_l, ans_mid);
+    if (right_ptr != q_id_r)
+        OverallDichotomy(right_ptr + 1, q_id_r, ans_mid + 1, ans_r);
 }
 int main() {
     scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; ++i) {
         scanf("%d", &a[i].x);
-        a[i].num = i;
+        a[i].id = i;
     }
     a[n + 1].x = 2e9;
-    sort(a + 1, a + 1 + n, cmp);
+    sort(a + 1, a + 1 + n);
     for (int i = 1; i <= m; ++i) {
-        scanf("%d%d%d", &q[i].x, &q[i].y, &q[i].k);
-        q[i].num = i;
+        scanf("%d%d%d", &q[i].l, &q[i].r, &q[i].k);
+        q[i].id = i;
     }
-    divide(1, m, -lim, lim);
+    OverallDichotomy(1, m, -lim, lim);
     for (int i = 1; i <= m; ++i)
         printf("%d\n", ans[i]);
     return 0;

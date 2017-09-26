f2a080e3c6347de02fe38d07c1548af1347f8f3f
godwings_thinkpad_desktop <godwings@outlook.com>
2017-09-26 15:01:38 +0800

    Update correct

--- test/correct.cpp
+++ test/correct.cpp
@@ -1,7 +1,45 @@
-/* URL: url */
-#include <cstdio>
-
+#include <bits/stdc++.h>
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define mo 1000000007
+#define N 60005
+int inv(int x) {
+    int y = mo - 2, s = 1;
+    while (y) {
+        if (y & 1)
+            s = 1ll * s * x % mo;
+        y >>= 1;
+        x = 1ll * x * x % mo;
+    }
+    return s;
+}
+int n, k, fac[N], ifac[N];
+int dp[N / 2][16], ans;
 int main() {
-
-    return 0;
+    char fni[] = "fun.in", fno[] = "fun.out";
+    freopen(fni, "r", stdin);
+    freopen(fno, "w", stdout);
+    scanf("%d%d", &n, &k);
+    fac[0] = ifac[0] = 1;
+    rep(i, 1, std::max(n, k))
+        ifac[i] = inv(fac[i] = 1ll * fac[i - 1] * i % mo);
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = 1;
+    ans = 0;
+    rep(i, 1, n / 2) {
+        rep(j, 1, std::min(k, 15)) {
+            int x = i;
+            while (x) {
+                dp[i][j] = (dp[i][j] + 1ll * ifac[x * 2] * dp[i ^ x][j - 1]) % mo;
+                x = (x - 1) & i;
+            }
+            ans = (ans + 1ll * dp[i][j] * fac[i * 2] % mo * fac[k] % mo * ifac[j] % mo * ifac[k - j]) % mo;
+        }
+    }
+    int all = 0;
+    while (n--)
+        all = 1ll * (all + 1) * k % mo;
+    printf("%d\n", (all - ans + mo) % mo);
+    fclose(stdin);
+    fclose(stdout);
 }

4e30132733f9765969680eb7e0eb1c313d3d1343
godwings_thinkpad_desktop <godwings@outlook.com>
2017-09-26 20:10:55 +0800

    Init

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 /*
-Name: title(sourceNo)
+Name: enemy(HDU1166)
 Author: godwings
-Date: DD/MM/YYYY
+Date: 26/09/2017
 */
 #include <cctype>
 #include <cstdio>
@@ -37,8 +37,8 @@ int main() {
 #ifdef _VISUAL_STUDIO
     freopen("test.in", "r", stdin);
 #else
-    freopen("title.in", "r", stdin);
-    freopen("title.out", "w", stdout);
+    freopen("enemy.in", "r", stdin);
+    freopen("enemy.out", "w", stdout);
 #endif  // _VISUAL_STUDIO
 #endif
 

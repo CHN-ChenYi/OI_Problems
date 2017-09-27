9bed51d3c2436f1898588e574b0aac094f26124d
godwings_thinkpad_desktop <godwings@outlook.com>
2017-09-27 21:03:36 +0800

    Finish BIT

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@ Date: 27/09/2017
 */
 #include <cctype>
 #include <cstdio>
+const int kMaxN = 1e5 + 10;
 
 namespace FastIO {
     template <class T>
@@ -31,6 +32,33 @@ namespace FastIO {
     }
 }  // namespace FastIO
 using FastIO::scan;
+
+struct BIT {
+private:
+    static const int MAXN = 1e5 + 10;
+    int n, tr[MAXN];
+    inline int lowbit(const int x) {
+        return x & -x;
+    }
+public:
+    void Init(const int n_) {
+        n = n_;
+    }
+    void Modify(int pos, int del) {
+        while (pos < n) {
+            tr[pos] += del;
+            pos += lowbit(pos);
+        }
+    }
+    int Query(int pos) {
+        int ret = 0;
+        while (pos) {
+            ret += tr[pos];
+            pos -= lowbit(pos);
+        }
+        return ret;
+    }
+};
 
 int main() {
 #ifndef ONLINE_JUDGE

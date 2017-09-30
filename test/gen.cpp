4d4887d17b2cfdf098074d04c81c66cc02abff65
godwings_thinkpad_desktop <godwings@outlook.com>
2017-09-30 21:05:17 +0800

    Finish gen

--- test/gen.cpp
+++ test/gen.cpp
@@ -6,15 +6,22 @@ inline int rand(const int lim) {
     return rand() * rand() % lim + 1;
 }
 
-const int T = 20;
-const int N = 10000;
-const int M = 10000;
+const int T = 15;
+const int N = 100000;
+const int M = 100000;
 
 int t, n, m;
 
 int main() {
     freopen("test.in", "w", stdout);
     srand(time(NULL));
-    
+    t = rand(T);
+    printf("%d\n", t);
+    while (t--) {
+        n = rand(N);
+        printf("%d\n", n);
+        for (int i = 1; i <= n; i++)
+            printf("%d %d %d\n", rand(M), rand(M), rand(M));
+    }
     return 0;
 }

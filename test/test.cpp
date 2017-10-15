db69d5d12e59284f970e5b4643384d1a4054342b
godwings_thinkpad_desktop <godwings@outlook.com>
2017-10-15 19:47:18 +0800

    Finish

--- test/test.cpp
+++ test/test.cpp
@@ -77,9 +77,27 @@ inline int gcd(int a, int b) {
     return a;
 }
 
+int ans;
 int n, m;
 char input;
 int matrix[kMaxN][kMaxN];
+int sum_d[kMaxN], sum_u[kMaxN];
+
+int Find(int x) {
+    int c = 0, z = 0;
+    for (int i = 2; i < m; i += 3) {
+        c += matrix[x][i];
+        z += matrix[x][i] - matrix[x][i - 1];
+    }
+    switch (m % 3) {
+        case 0:
+            return c;
+        case 1:
+            return c + matrix[x][m] - z;
+        default:
+            return c + matrix[x][m];
+    }
+}
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -91,12 +109,29 @@ int main() {
 #endif  // _VISUAL_STUDIO
 #endif  // ONLINE_JUDGE
     scan(n, m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
             scan(input);
             matrix[i][j] = input - '0';
         }
     }
-
+    for (int i = 2; i < n; i += 3)
+        sum_u[i + 1] = sum_u[i - 2] + Find(i) - Find(i - 1);
+    for (int i = n - 1; i > 1; i -= 3)
+        sum_d[i - 1] = sum_d[i + 2] + Find(i) - Find(i + 1);
+    switch (n % 3) {
+        case 2:
+            ans = sum_u[(n + 1) >> 1];
+            break;
+        case 1:
+            ans = Find((n + 1) >> 1) - sum_u[n >> 1] - sum_d[(n + 3) >> 1];
+            break;
+        default:
+            ans = Find((n + 1) >> 1) - sum_d[n >> 1] - sum_u[(n + 3) >> 1];
+            break;
+    }
+    ans = m - ans;
+    const int k = gcd(ans, m);
+    printf("%d/%d\n", ans / k, m / k);
     return 0;
 }

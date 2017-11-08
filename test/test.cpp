db155062cd94e9165aad2e3228107fe007bee54e
godwings_thinkpad_desktop <godwings@outlook.com>
2017-11-08 19:35:04 +0800

    make FastIO::c and FastIO::positive global change intend from 4 to 2

--- test/test.cpp
+++ test/test.cpp
@@ -7,73 +7,73 @@ Date: DD/MM/YYYY
 
 #include <cctype>
 namespace FastIO {
-    template <class T_1>
-    inline void scan(T_1 &x) {
-        x = 0;
-        char c;
-        bool positive = 1;
-        while (!isdigit(c = getchar())) {
-            if (!positive) positive = 1;
-            if (c == '-') positive = 0;
-        }
-        while (isdigit(c)) {
-            x = x * 10 + (c & 0xf);
-            c = getchar();
-        }
-        if (!positive)
-            x *= -1;
+  char c;
+  bool positive;
+  template <class T_1>
+  inline void scan(T_1 &x) {
+    x = 0;
+    positive = 1;
+    while (!isdigit(c = getchar())) {
+      if (!positive) positive = 1;
+      if (c == '-') positive = 0;
     }
-    inline void scan(char &x) {
-        while (!isgraph(x = getchar())) { }
+    while (isdigit(c)) {
+      x = x * 10 + (c & 0xf);
+      c = getchar();
     }
-    inline void scan(double &x) {
-        x = 0;
-        char c;
-        bool positive = 1;
-        while (!isdigit(c = getchar())) {
-            if (!positive) positive = 1;
-            if (c == '-') positive = 0;
-        }
-        while (isdigit(c)) {
-            x = x * 10 + (c & 0xf);
-            c = getchar();
-        }
-        if (c == '.') {
-            double now = 0.1;
-            c = getchar();
-            while (isdigit(c)) {
-                x += now * (c & 0xf);
-                now *= 0.1;
-                c = getchar();
-            }
-        }
-        if (!positive)
-            x *= -1;
+    if (!positive)
+      x *= -1;
+  }
+  inline void scan(char &x) {
+    while (!isgraph(x = getchar())) { }
+  }
+  inline void scan(double &x) {
+    x = 0;
+    positive = 1;
+    while (!isdigit(c = getchar())) {
+      if (!positive) positive = 1;
+      if (c == '-') positive = 0;
     }
-    template <class T_1, class T_2>
-    inline void scan(T_1 &a, T_2 &b) {
-        scan(a); scan(b);
+    while (isdigit(c)) {
+      x = x * 10 + (c & 0xf);
+      c = getchar();
     }
-    template <class T_1, class T_2, class T_3>
-    inline void scan(T_1 &a, T_2 &b, T_3 &c) {
-        scan(a); scan(b); scan(c);
+    if (c == '.') {
+      double now = 0.1;
+      c = getchar();
+      while (isdigit(c)) {
+        x += now * (c & 0xf);
+        now *= 0.1;
+        c = getchar();
+      }
     }
-    template <class T_1, class T_2, class T_3, class T_4>
-    inline void scan(T_1 &a, T_2 &b, T_3 &c, T_4 &d) {
-        scan(a); scan(b); scan(c); scan(d);
-    }
+    if (!positive)
+      x *= -1;
+  }
+  template <class T_1, class T_2>
+  inline void scan(T_1 &a, T_2 &b) {
+    scan(a); scan(b);
+  }
+  template <class T_1, class T_2, class T_3>
+  inline void scan(T_1 &a, T_2 &b, T_3 &c) {
+    scan(a); scan(b); scan(c);
+  }
+  template <class T_1, class T_2, class T_3, class T_4>
+  inline void scan(T_1 &a, T_2 &b, T_3 &c, T_4 &d) {
+    scan(a); scan(b); scan(c); scan(d);
+  }
 }  // namespace FastIO
 using FastIO::scan;
 
 int main() {
 #ifndef ONLINE_JUDGE
 #ifdef _VISUAL_STUDIO
-    freopen("test.in", "r", stdin);
+  freopen("test.in", "r", stdin);
 #else
-    freopen("title.in", "r", stdin);
-    freopen("title.out", "w", stdout);
+  freopen("title.in", "r", stdin);
+  freopen("title.out", "w", stdout);
 #endif  // _VISUAL_STUDIO
 #endif  // ONLINE_JUDGE
 
-    return 0;
+  return 0;
 }
